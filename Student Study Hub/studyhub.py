349f2b738d7536071cd9962a9d4133fa5c070200
Muhammad Rahimoon <146037148+myr03@users.noreply.github.com>
2025-11-05 22:47:50 -0500

    Update studyhub.py

--- Student Study Hub/studyhub.py
+++ Student Study Hub/studyhub.py
@@ -1,1 +1,21 @@
+import tkinter
+
+class StudySession:
+    def __init__(self, users, privacy, courseID, description, members):
+        self.users = users
+        self.courseID = courseID
+        self.description = description
+        self.members = members
+        self.privacy = privacy
+
+def invite(self,user):
+    pass
+def leave(self, user):
+    pass
+def getDescription(self):
+    pass
+def setDescription(self):
+    pass
+
+
 

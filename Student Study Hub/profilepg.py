3cafb852ea3e9f055a8023720fadf5beb672e2e4
Peter Pax <ppax56@gmail.com>
2025-11-19 14:03:08 -0500

    commit

--- Student Study Hub/profilepg.py
+++ Student Study Hub/profilepg.py
@@ -1,18 +1,17 @@
-import tkinter
+from dataclasses import dataclass, field
 
+@dataclass
 class Profile:
-    def __init__(self, username, email, courses):
-        self.username = username
-        self.email = email
-        self.courses = courses
+    username: str
+    email: str
+    courses: list[str] = field(default_factory=list)
 
-    def update_email(self, new_email):
+    def update_email(self, new_email: str) -> None:
         self.email = new_email
 
-    def add_course(self, course):
-        if course not in self.courses:
-            self.courses.append(course)
+    def add_course(self, course_name: str) -> None:
+        self.courses.append(course_name)
 
-    def remove_course(self, course):
-        if course in self.courses:
-            self.courses.remove(course)
+    def remove_course(self, course_name: str) -> None:
+        if course_name in self.courses:
+            self.courses.remove(course_name)

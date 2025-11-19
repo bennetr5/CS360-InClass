9a1e2ca48bf94a8295ee50ed4f06f3ade0eb684e
bennetr5 <122103056+bennetr5@users.noreply.github.com>
2025-11-19 14:29:15 -0500

    refactoring

--- notes/note.py
+++ notes/note.py
@@ -1,16 +1,24 @@
 import tkinter as tkinter
-
+import profilepg as profile
+import os
+import json 
 
 class note:
-    def __init__(self, title, content, id, user):
+    def __init__(self, title, content, noteId, user):
         self.title = title
         self.content = content
-        self.id = id
+        self.noteId = noteId
         self.user = user
 
-    def save():
-        # TO-DO Save Notes
-        return "No Functionality"
+    def save(self):
+        directory = "./contents"
+        fileName = str(self.title) + ".json"
+        fullPath = os.path.join(directory, fileName)
+        data = {
+            "content": self.content
+        }
+        with open(fullPath, 'w', encoding='utf=8') as json_file:
+            json.dump(data, json_file, indent=4)
     
     # Getters and Setter
     def getTitle(self):
